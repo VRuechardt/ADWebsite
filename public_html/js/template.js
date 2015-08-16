298889f70e6f86aa6edcfd340cd650448730bb3c
vruechardt <valentin.ruechardt@cdtm.de>
2015-08-16 16:31:43 +0200

    reset images

---
+++ public_html/js/template.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    window.setTimeout(function() {
+        $("#splash").addClass("splash-animated");
+    }, 500);
+    
+    var pages = ["sample"];
+    var pageData = [];
+    $(pages).each(function(i, e) {
+        var index = i;
+        $.get("html/" + e + ".html", function(data) {
+            
+            pageData[index] = data;
+            
+            var done = true;
+            for(var c = 0; c < pages.length; c++) {
+                if(!pageData[c]) {
+                    done = false;
+                }
+            }
+            
+            if(done) {
+                var html = "";
+                for(var i in pageData) {
+                    html += pageData[i];
+                }
+                $("#container").html(html);
+            }
+        });
+    });
+    
+});
+hljs.initHighlightingOnLoad();
